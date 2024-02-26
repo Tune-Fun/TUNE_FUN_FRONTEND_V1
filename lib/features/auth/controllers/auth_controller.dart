1ece22d934e8a3e240aa2af44ee4866c3be391d6
seilylook <seilylook@naver.com>
2024-02-26 12:50:53 +0900

    refactor: user sign up request

--- lib/features/auth/controllers/auth_controller.dart
+++ lib/features/auth/controllers/auth_controller.dart
@@ -1,32 +1,60 @@
-import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
+import 'package:tunefun_front/apis/apis.dart';
+import 'package:tunefun_front/common/common.dart';
 import 'package:tunefun_front/models/models.dart';
 
 var logger = Logger();
 
 final authControllerProvider =
     StateNotifierProvider<AuthController, bool>((ref) {
-  return AuthController();
+  return AuthController(
+    authAPI: ref.watch(authAPIProvider),
+  );
 });
 
 class AuthController extends StateNotifier<bool> {
-  AuthController() : super(false);
+  final AuthAPI _authAPI;
 
-  Future<void> signup({required UserModel userModel}) async {
-    try {
-      final userAttributes = {AuthUserAttributeKey.name: userModel.username};
+  AuthController({required AuthAPI authAPI})
+      : _authAPI = authAPI,
+        super(false);
 
-      await Amplify.Auth.signUp(
-        username: userModel.email,
-        password: userModel.password,
-        options: SignUpOptions(
-          userAttributes: userAttributes,
-        ),
-      );
-    } on AuthException catch (e) {
-      logger.e(e);
-      return;
-    }
+  Future<void> signup({
+    required String email,
+    required String username,
+    required String password,
+    required String nickname,
+    required String accountType,
+    required BuildContext context,
+  }) async {
+    state = true;
+
+    final res = await _authAPI.signup(
+      email: email,
+      username: username,
+      password: password,
+      nickname: nickname,
+      accountType: accountType,
+    );
+
+    state = false;
+
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) async {
+        UserModel userModel = UserModel(
+          email: email,
+          username: username,
+          password: password,
+          nickname: nickname,
+          accountType: accountType,
+        );
+
+        // repository에 전달해주고
+        // 사용자 데이터를 DB에 저장해주는 코드를 추가해준다.
+      },
+    );
   }
 }
