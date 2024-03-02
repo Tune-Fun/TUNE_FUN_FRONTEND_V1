157c1fa22c760ae5c01dfab3cfb9a0cb0c20a59f
rcshpark <rcshpark@gmail.com>
2024-03-02 20:06:16 +0900

    design : widget 분리

---
+++ lib/features/vote/widgets/dotted_box.dart
@@ -0,0 +1,33 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+
+class DottedBorderBox extends StatelessWidget {
+  const DottedBorderBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('123');
+      },
+      child: DottedBorder(
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(5),
+        padding: const EdgeInsets.all(6),
+        dashPattern: const [8, 4],
+        strokeWidth: 2,
+        color: Colors.black,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          child: Container(
+            padding: const EdgeInsets.only(top: 10, left: 10),
+            height: MediaQuery.of(context).size.height * 0.05,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            child: const Text('노래 추가하여 투표...'),
+          ),
+        ),
+      ),
+    );
+  }
+}
