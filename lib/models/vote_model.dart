b9b95e6160565e48c35dab1ec103de3b70a94835
rcshpark <rcshpark@gmail.com>
2024-03-03 17:50:40 +0900

    feat : vote upload model

--- lib/models/vote_model.dart
+++ lib/models/vote_model.dart
@@ -1,39 +1,28 @@
 class VoteListModel {
-  late String title;
-  late String uid;
-  late String profile;
-  late String content;
-  late String profileImage;
-  late int likeCnt;
-  late int votesCnt;
+  late String votePaperId;
+  late String? selectedOption;
+  late String username;
   late DateTime createdAt;
   late DateTime updatedAt;
-  late DateTime expiredAt;
+  late DateTime deletedAt;
 
   VoteListModel.fromJson(Map<String, dynamic> json) {
-    title = json["title"];
-    profile = json["profile"];
-    uid = json["uid"];
-    content = json["content"];
-    profileImage = json["profileImage"];
-    likeCnt = json["likeCnt"];
-    votesCnt = json["votesCnt"];
+    votePaperId = json["votePaperId"];
+    selectedOption = json["selectedOption"];
+    username = json["username"];
     createdAt = json["createdAt"];
     updatedAt = json["updatedAt"];
-    expiredAt = json["expiredAt"];
+    deletedAt = json["deletedAt"];
   }
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['title'] = title;
-    data['profile'] = profile;
-    data['uid'] = uid;
-    data['content'] = content;
-    data['profileImage'] = profileImage;
-    data['likeCnt'] = likeCnt;
-    data['votesCnt'] = votesCnt;
+    data['votePaperId'] = votePaperId;
+    data['selectedOption'] = selectedOption;
+    data['username'] = username;
+    data['createdAt'] = createdAt;
     data['createdAt'] = createdAt;
     data['updatedAt'] = updatedAt;
-    data['expiredAt'] = expiredAt;
+    data['deletedAt'] = deletedAt;
     return data;
   }
 }
