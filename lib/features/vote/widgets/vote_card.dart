80158370fca4b1c5f8aa18abf1b2d6f9ef30f167
rcshpark <rcshpark@gmail.com>
2024-03-04 17:31:58 +0900

    feat : 투표리스트 위젯 분리

--- lib/features/vote/widgets/vote_card.dart
+++ lib/features/vote/widgets/vote_card.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class VoteCard extends ConsumerWidget {
-  const VoteCard({super.key});
+final clickedIndexProvider = StateProvider<int?>((ref) => null);
 
+class VoteCard extends ConsumerWidget {
+  final int index;
+  final Map<String, dynamic> data;
+  const VoteCard({required this.index, required this.data, super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Placeholder();
+    final clickedIndex = ref.watch(clickedIndexProvider);
+    return Material(
+      child: GestureDetector(
+          onTap: () {
+            ref.read(clickedIndexProvider.notifier).state =
+                clickedIndex == index ? null : index;
+          },
+          child: Container(
+            margin: const EdgeInsets.only(top: 10),
+            height: MediaQuery.of(context).size.height * 0.08,
+            width: MediaQuery.of(context).size.width,
+            child: Card(
+              elevation: 0,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(data["artist"]),
+                    const Text(" - "),
+                    Text(data["song"]),
+                    const Spacer(),
+                    clickedIndex == index
+                        ? const Icon(Icons.check)
+                        : const SizedBox(),
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
   }
 }
