9ebb24c8f08349a254237764e7112f065c22996d
rcshpark <rcshpark@gmail.com>
2024-03-13 16:54:31 +0900

    feat: 노래 검색 api 연결

--- lib/repository/vote_repository.dart
+++ lib/repository/vote_repository.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:tunefun_front/constants/url_constants.dart';
+import 'package:tunefun_front/features/vote/controller/token_controller.dart';
 import 'package:tunefun_front/models/vote_paper_model.dart';
 
-class DataSource {
+class VoteDataSource {
   Future<List<VotePaperModel>> getVotesList() async {
     final response = await http.get(Uri.parse(''));
     if (response.statusCode == 200) {
@@ -17,5 +19,23 @@ class DataSource {
 
   uploadvote() async {
     final response = await http.post(Uri.parse(""));
+  }
+
+  searchSongByArtist(String artist) async {
+    final accessTokenManager = AccessTokenManager();
+    final accessToken = await accessTokenManager.getAccessToken();
+    final searchUrl = "${UrlConstants.spotifyURL}/search?q=$artist&type=track";
+    final response = await http.get(
+      Uri.parse(searchUrl),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final tracks = data['tracks']['items'];
+      return tracks;
+    } else {
+      throw Exception('Failed to search songs');
+    }
   }
 }
