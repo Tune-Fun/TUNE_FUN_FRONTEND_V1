dc57e116df9ec3f1c102fc4c7c4a0f765018784a
rcshpark <rcshpark@gmail.com>
2024-03-15 15:19:07 +0900

    feat: mock data , upload test model

---
+++ lib/features/vote/test/search_mock.dart
@@ -0,0 +1,82 @@
+List searchMock = [
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "Test 1",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "아티스트 A", "id": "1"}
+    ]
+  },
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "Test 2",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "TEST", "id": "1"}
+    ]
+  },
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "Test 3",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "aaa", "id": "1"}
+    ]
+  },
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "asdasdsa",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "A", "id": "1"}
+    ]
+  },
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "Test 1",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "아티스트 A", "id": "1"}
+    ]
+  },
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "Test 1",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "아티스트 A", "id": "1"}
+    ]
+  },
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "Test 1",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "아티스트 A", "id": "1"}
+    ]
+  },
+  {
+    "album": {
+      "total_tracks": 10,
+      "name": "Test 1",
+      "release_date": "2023-01-01"
+    },
+    "artists": [
+      {"name": "아티스트 A", "id": "1"}
+    ]
+  }
+];
