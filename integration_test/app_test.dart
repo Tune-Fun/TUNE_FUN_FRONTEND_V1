2e42813a3dd058a1430269d8f50af6304e83c2c3
seilylook <seilylook@naver.com>
2024-03-25 11:57:12 +0900

    config: CIpipeline with Patrol & Gcloud & Firebase Test lab

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,1 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+import 'package:tunefun_front/constants/dummy_data.dart';
+import 'package:tunefun_front/features/article/widgets/article_card.dart';
+import 'package:tunefun_front/features/article/widgets/article_list.dart';
+import 'package:tunefun_front/features/home/views/home_view.dart';
+import 'package:tunefun_front/main.dart';
+import 'package:patrol_finders/patrol_finders.dart';
 
+void main() {
+  testWidgets(
+    'Main Screen test',
+    (WidgetTester tester) async {
+      PatrolTester $ = PatrolTester(
+        tester: tester,
+        config: const PatrolTesterConfig(),
+      );
+      await $.pumpWidget(
+        const ProviderScope(
+          child: MyApp(),
+        ),
+      );
+      expect($('TuneFun'), findsWidgets);
+      expect($(const HomeScreen()), findsOneWidget);
+      expect($(const ArticleList()), findsWidgets);
+    },
+  );
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,32 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:tunefun_front/constants/dummy_data.dart';
+import 'package:tunefun_front/features/article/widgets/article_card.dart';
+import 'package:tunefun_front/features/article/widgets/article_list.dart';
+import 'package:tunefun_front/features/home/views/home_view.dart';
 import 'package:tunefun_front/main.dart';
+import 'package:patrol_finders/patrol_finders.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  testWidgets(
+    'Main Screen test',
+    (WidgetTester tester) async {
+      PatrolTester $ = PatrolTester(
+        tester: tester,
+        config: const PatrolTesterConfig(),
+      );
+      await $.pumpWidget(
+        const ProviderScope(
+          child: MyApp(),
+        ),
+      );
+      expect($('TuneFun'), findsWidgets);
+      expect($(const HomeScreen()), findsOneWidget);
+      expect($(const ArticleList()), findsWidgets);
+      final articles = DummyData.articles;
+    },
+  );
 }
