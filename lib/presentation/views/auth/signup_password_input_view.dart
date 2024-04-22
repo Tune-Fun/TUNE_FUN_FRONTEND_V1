2869d2bccc1d2c8a8ec96560f6d5e7c9d7ec4da3
rcshpark <rcshpark@gmail.com>
2024-04-22 15:08:58 +0900

    refactor: password ui (12300)

--- lib/presentation/views/auth/signup_password_input_view.dart
+++ lib/presentation/views/auth/signup_password_input_view.dart
@@ -29,12 +29,18 @@ class _SignupPasswordInputScreenState extends State<SignupPasswordInputScreen> {
   final passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   bool buttonState = false;
-
+  String? errorText;
   @override
   void initState() {
     super.initState();
     emailController = widget.emailController;
     usernameController = widget.usernameController;
+  }
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
   }
 
   @override
@@ -53,72 +59,76 @@ class _SignupPasswordInputScreenState extends State<SignupPasswordInputScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
-                    '비밀번호 만들기',
-                    style: TextStyle(
-                      color: Pallete.textMainColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '비밀번호 만들기',
+                        style: TextStyle(
+                          color: Pallete.textMainColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      if (errorText != null) // 에러 메시지를 조건부로 표시합니다.
+                        Text(
+                          errorText!,
+                          style: TextStyle(color: Colors.red, fontSize: 14),
+                        ),
+                    ],
                   ),
                   const SizedBox(height: 12),
                   TextFormField(
                     controller: passwordController,
                     decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: const BorderSide(
-                            color: Color.fromRGBO(234, 234, 234, 1),
-                            width: 1,
-                          ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(234, 234, 234, 1),
+                          width: 1,
                         ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: const BorderSide(
-                            color: Color.fromRGBO(234, 234, 234, 1),
-                            width: 1,
-                          ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(234, 234, 234, 1),
+                          width: 1,
                         ),
-                        errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: const BorderSide(
-                            color: Color.fromRGBO(234, 234, 234, 1),
-                            width: 1,
-                          ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(234, 234, 234, 1),
+                          width: 1,
                         ),
-                        focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: const BorderSide(
-                            color: Color.fromRGBO(234, 234, 234, 1),
-                            width: 1,
-                          ),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(234, 234, 234, 1),
+                          width: 1,
                         ),
-                        contentPadding: const EdgeInsets.all(22),
-                        hintText: '비밀번호',
-                        hintStyle: const TextStyle(
-                          fontSize: 18,
-                        ),
-                        helperText: "8가지 이상 영문/숫자/특수문자 중2가지 포함"),
+                      ),
+                      contentPadding: const EdgeInsets.all(22),
+                      hintText: '비밀번호',
+                      hintStyle: const TextStyle(
+                        fontSize: 18,
+                      ),
+                      helperStyle: const TextStyle(
+                          color: Color.fromRGBO(153, 153, 153, 1)),
+                      helperText: "8가지 이상 영문/숫자/특수문자 중2가지 포함",
+                      errorStyle: const TextStyle(
+                          color: Color.fromRGBO(153, 153, 153, 1)),
+                    ),
                     keyboardType: TextInputType.text,
                     obscureText: true,
-                    onChanged: (value) {
-                      setState(() {});
-                    },
+                    onChanged: (value) => setState(() {}),
                     validator: (value) {
-                      if (value!.isEmpty) {
-                        passwordController.clear();
-                        return '비밀번호를 입력해주세요.';
-                      }
-                      if (value.length <= 8) {
-                        passwordController.clear();
-                        return "8가지 이상 영문/숫자/특수문자 중2가지 포함";
-                      }
+                      if (value!.isEmpty) return "8가지 이상 영문/숫자/특수문자 중2가지 포함";
+                      if (value.length <= 8) return "8가지 이상 영문/숫자/특수문자 중2가지 포함";
                       if (!RegExp(
-                              r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?~^<>,.&+=])[A-Za-z\d$@$!%*#?~^<>,.&+=]{8,15}$')
-                          .hasMatch(value)) {
-                        passwordController.clear();
-                        return "8가지 이상 영문/숫자/특수문자 중2가지 포함";
-                      }
+                              r'^(?=(?:.*[A-Za-z].*[0-9])|(?:.*[A-Za-z].*[$@$!%*#?&])|(?:.*[0-9].*[$@$!%*#?&]))[A-Za-z0-9$@$!%*#?&]{8,40}$')
+                          .hasMatch(value)) return "8가지 이상 영문/숫자/특수문자 중2가지 포함";
                       return null;
                     },
                   ),
@@ -145,6 +155,10 @@ class _SignupPasswordInputScreenState extends State<SignupPasswordInputScreen> {
                                 ),
                               ),
                             );
+                          } else {
+                            setState(() {
+                              errorText = '비밀번호가 취약합니다.';
+                            });
                           }
                         },
                         buttonState: passwordController.text.isNotEmpty,
