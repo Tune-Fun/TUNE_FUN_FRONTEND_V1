839fade3903f0c2599007ae79765a04d0ed93b31
rcshpark <rcshpark@gmail.com>
2024-06-05 13:24:17 +0900

    feat: access token 처리

--- lib/features/vote/data/data_source/vote_data_source.dart
+++ lib/features/vote/data/data_source/vote_data_source.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:tunefun_front/constants/url_constants.dart';
 import 'package:tunefun_front/features/vote/domain/model/upload_model.dart';
@@ -14,7 +15,8 @@ class VoteDataSource {
   VoteDataSource(this.ref);
 
   uploadvote(UploadModel uploadModel) async {
-    String accessToken = '';
+    const storage = FlutterSecureStorage();
+    final accessToken = await storage.read(key: 'access_token');
     final uploadURL = UrlConstants.voteUploadURL;
     final response = await http.post(Uri.parse(uploadURL),
         headers: {
