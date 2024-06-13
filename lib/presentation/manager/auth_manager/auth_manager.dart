a424ab5251fc784e2db0bb06df2bcabe4a97a452
rcshpark <rcshpark@gmail.com>
2024-06-13 13:48:10 +0900

    feat: user manager에 데이터 부여

--- lib/presentation/manager/auth_manager/auth_manager.dart
+++ lib/presentation/manager/auth_manager/auth_manager.dart
@@ -1,23 +1,29 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:tunefun_front/domain/model/account_model.dart';
 import 'package:tunefun_front/domain/usecase/auth_usecase_impl.dart';
+import 'package:tunefun_front/presentation/manager/auth_manager/user_manager.dart';
 
 part 'auth_manger_state.dart';
 
 final authManagerProvider =
     StateNotifierProvider<AuthManager, AuthMangerState>((ref) {
   final authUsecase = ref.watch(authUsecaseProvider);
-  return AuthManager(authUsecase);
+  final userManager = ref.watch(userManagerProvider.notifier);
+  return AuthManager(authUsecase, userManager);
 });
 
 class AuthManager extends StateNotifier<AuthMangerState> {
   final AuthUsecaseImpl _authUsecaseImpl;
-  AuthManager(this._authUsecaseImpl) : super(const AuthMangerStateInitial());
+  final UserManager _userManager;
+  AuthManager(this._authUsecaseImpl, this._userManager)
+      : super(const AuthMangerStateInitial());
 
   Future<void> signUp(Map<String, dynamic> params) async {
     final response = await _authUsecaseImpl.signUp(params);
     response.when(success: (userInfo) {
       state = AuthMangerStateSuccess(userInfo);
+      _userManager.setUser(userInfo);
     }, error: (error, message) {
       state = AuthMangerStateError(message);
     });
@@ -26,8 +32,12 @@ class AuthManager extends StateNotifier<AuthMangerState> {
   Future<void> logIn(dynamic params) async {
     state = const AuthManagerStateLoading();
     final response = await _authUsecaseImpl.logIn(params);
-    response.when(success: (userInfo) {
+    response.when(success: (userInfo) async {
       state = AuthMangerStateSuccess(userInfo);
+      _userManager.setUser(userInfo);
+      const storage = FlutterSecureStorage();
+      await storage.write(key: 'access_token', value: userInfo.accessToken);
+      await storage.write(key: 'refresh_token', value: userInfo.refreshToken);
     }, error: (error, message) {
       state = AuthMangerStateError(message);
     });
