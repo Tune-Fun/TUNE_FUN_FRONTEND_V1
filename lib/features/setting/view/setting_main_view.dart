110c829cdfcc963bc29d5900936c93fa43d4f4e6
rcshpark <rcshpark@gmail.com>
2024-07-29 16:00:57 +0900

    feat : 설정 및 개인정보 화면 퍼블리싱

---
+++ lib/features/setting/view/setting_main_view.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:tunefun_front/features/setting/view/account_setting_view.dart';
+import 'package:tunefun_front/features/setting/view/agreement/agreement_detail_view.dart';
+import 'package:tunefun_front/features/setting/view/agreement/agreement_view.dart';
+
+class SettingMainView extends StatelessWidget {
+  const SettingMainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(249, 249, 249, 1),
+      appBar: AppBar(
+        title: const Text(
+          '설정 및 개인정보',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              margin: const EdgeInsets.only(top: 20),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SectionHeader(title: '정보'),
+                  SettingsListTile(
+                      title: '계정 정보',
+                      onTap: () {
+                        onAccountInfoTap(context);
+                      }),
+                  const SettingsListTile(
+                      title: '알림 설정', onTap: _onNotificationSettingsTap),
+                  const SettingsListTile(
+                      title: '결제 내역', onTap: _onPaymentHistoryTap),
+                  SettingsListTile(
+                      title: '약관 및 정책',
+                      onTap: () {
+                        onTermsAndPolicyTap(context);
+                      }),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              margin: const EdgeInsets.only(top: 20),
+              width: MediaQuery.of(context).size.width,
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SectionHeader(title: '도움말'),
+                  SettingsListTile(title: '신고', onTap: _onReportTap),
+                  SettingsListTile(title: '고객센터', onTap: _onCustomerServiceTap),
+                  SettingsListTile(title: '이용문의', onTap: _onInquiriesTap),
+                  SettingsListTile(title: '웹 버전', onTap: _onWebVersionTap),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              margin: const EdgeInsets.only(top: 20),
+              width: MediaQuery.of(context).size.width,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: ListTile(
+                  title: Text(
+                    '로그아웃',
+                    style: TextStyle(
+                      color: Color.fromRGBO(17, 17, 17, 1),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onTap: _onLogoutTap,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onAccountInfoTap(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const AccountSettingScreen()));
+  }
+
+  static void _onNotificationSettingsTap() {
+    // 알림 설정 페이지로 이동하는 로직 추가
+  }
+
+  static void _onPaymentHistoryTap() {
+    // 결제 내역 페이지로 이동하는 로직 추가
+  }
+
+  void onTermsAndPolicyTap(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const AgreementScreen()));
+  }
+
+  static void _onReportTap() {
+    // 신고 페이지로 이동하는 로직 추가
+  }
+
+  static void _onCustomerServiceTap() {
+    // 고객센터 페이지로 이동하는 로직 추가
+  }
+
+  static void _onInquiriesTap() {
+    // 이용문의 페이지로 이동하는 로직 추가
+  }
+
+  static void _onWebVersionTap() {
+    // 웹 버전 페이지로 이동하는 로직 추가
+  }
+
+  static void _onLogoutTap() {
+    // 로그아웃 로직 추가
+  }
+}
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+
+  const SectionHeader({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        title,
+        style: const TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 22,
+            color: Color.fromRGBO(17, 17, 17, 1)),
+      ),
+    );
+  }
+}
+
+class SettingsListTile extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+
+  const SettingsListTile({
+    required this.title,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: ListTile(
+        title: Text(
+          title,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        ),
+        trailing: const Icon(Icons.arrow_forward_ios,
+            size: 16, color: Color.fromRGBO(102, 102, 102, 1)),
+        onTap: onTap,
+      ),
+    );
+  }
+}
