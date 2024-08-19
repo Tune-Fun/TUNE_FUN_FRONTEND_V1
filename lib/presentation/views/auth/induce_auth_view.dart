3d7765923387246bcbe2a6951d227ff7b6747fe8
rcshpark <rcshpark@gmail.com>
2024-08-19 15:44:07 +0900

    feat : 사용자 로그인 유도 화면 생성

---
+++ lib/presentation/views/auth/induce_auth_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tunefun_front/constants/constants.dart';
+import 'package:tunefun_front/features/vote/presentation/widgets/gradient_container.dart';
+import 'package:tunefun_front/presentation/views/auth/login_view.dart';
+import 'package:tunefun_front/presentation/views/auth/signup_main_view.dart';
+
+class InduceAuthScreen extends StatelessWidget {
+  const InduceAuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildBody(context);
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(ImageConstants.induceAuthIcon),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()));
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 16, right: 16, top: 50, bottom: 30),
+              child: GradientContainer(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  borderRadius: BorderRadius.circular(100),
+                  type: "fill",
+                  child: const Center(
+                      child: Text(
+                    "로그인",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 22,
+                        color: Colors.white),
+                  ))),
+            ),
+          ),
+          GestureDetector(
+            onTap: () async {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SignUpMainScreen()));
+            },
+            child: const Text(
+              "회원가입",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  color: Colors.black),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
