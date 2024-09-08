238e13d2f76a2a9034724583e6bdd16bcd44a176
rcshpark <rcshpark@gmail.com>
2024-09-08 21:50:08 +0900

    refactor : 색상 컴포넌트 변경

--- lib/theme/pallete.dart
+++ lib/theme/pallete.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Pallete {
-  static const Color bgMainColor = Colors.white;
+  static const Color bgMainColor = Color.fromRGBO(255, 255, 255, 1);
   static const Color bgSubColor = Color.fromRGBO(249, 249, 249, 1);
   static const Color textMainColor = Color.fromRGBO(17, 17, 17, 1);
   static const Color textSubColor = Color.fromRGBO(102, 102, 102, 1);
@@ -10,7 +10,7 @@ class Pallete {
   static const Color greenColor = Color.fromRGBO(102, 216, 97, 1);
   static const Color pointColor = Color.fromRGBO(251, 92, 102, 1);
   static const Color pinkColor = Color.fromRGBO(251, 92, 102, 1);
-  static const Color redColor = Color.fromRGBO(250, 35, 48, 1);
+  static const Color redColor = Color.fromRGBO(233, 20, 20, 1);
   static const Color borderColor = Color.fromRGBO(234, 234, 234, 1);
   static const Color bgCustomColor = Color.fromRGBO(255, 242, 242, 1);
   // static const Color color1 = Color.fromRGBO(250, 92, 102, 1);
